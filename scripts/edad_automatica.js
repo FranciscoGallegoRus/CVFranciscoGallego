2cffc38bbf063bbf117e61de39851f1d1cffedee
Francisco Gallego Rus <frangr2004@gmail.com>
2024-11-15 14:34:49 +0100

    Add files via upload

---
+++ scripts/edad_automatica.js
@@ -0,0 +1,16 @@
+function calcularEdad(fechaNacimiento) {
+    const hoy = new Date();
+    const nacimiento = new Date(fechaNacimiento);
+    let edad = hoy.getFullYear() - nacimiento.getFullYear();
+    const mes = hoy.getMonth() - nacimiento.getMonth();
+    if (mes < 0 || (mes === 0 && hoy.getDate() < nacimiento.getDate())) {
+        edad--;
+    }
+    return edad;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fechaNacimiento = '2004-09-07';
+    const edad = calcularEdad(fechaNacimiento);
+    document.getElementById('edad').textContent = edad;
+});
